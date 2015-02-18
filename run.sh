455d12ab7530a908eed6aca1b13666b9f941d0e7
bogdannechyporenko <bnechyporenko@xebia.com>
2015-02-18 08:15:02 +0100

    Updated run script

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --load-and-launch-app=/Users/bogdannechyporenko/proj/chrome-extensions/hello-world
+cur=`pwd`
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --load-and-launch-app=$cur
